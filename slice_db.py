a0d49721c575c1401ea1c4da04c014b95aaed39f
h-satogata <>
2019-12-25 18:59:48 +0900

    author_dateごとにflagをつける処理を追加

--- slice_db.py
+++ slice_db.py
@@ -6,6 +6,7 @@ conn_2 = sqlite3.connect('hadoop.db')
 cursor_2 = conn_2.cursor()
 
 def slice_db():
+    timestamp_target = ""
     # 行数を獲得する
     try:
         cursor_1.execute("SELECT COUNT(tag) FROM hadoop_hash_tag_date")
@@ -15,19 +16,26 @@ def slice_db():
     except sqlite3.Error as e:
         print('sqlite3.Error occurred:', e.args[0])
 
+    # 1つ目のリリース以前のものにフラグをつける
+    try:
+        cursor_1.execute("SELECT author_date_unix_timestamp FROM hadoop_hash_tag_date WHERE id = %d", 1)
+        timestamp_target_tuple = cursor_1.fetchone()
+        timestamp_target = timestamp_target_tuple[0]
+        cursor_2.execute("UPDATE data SET author_date_flag = '1' WHERE author_date_unix_timestamp < %s"
+                         , timestamp_target)
 
-    # dbをリリースごとにスライスする。author_dateを用いる
+    except sqlite3.Error as e:
+        print('sqlite3.Error occurred:', e.args[0])
+
+
+    # dbをリリースごとにフラグをつける。author_date_unix_timestampを用いる
     for loop_num in range(0, column_num):
         try:
-            cursor_1.execute("SELECT tag FROM hadoop_hash_tag_date WHERE ID = %d", column_num)
-
-            key_list = sorted(list(hash_dict.keys()))
-            #        print(len(hash_dict))
-            #        print(len(set([hash_dict[key] for key in key_list])))
-            data = [{'hash': hash_dict[key], 'tag': tag_dict[key], 'author_date': authordate_dict[key]} for key in
-                    key_list]
-            cursor.executemany("INSERT INTO hadoop_hash_tag_date VALUES (:hash, :tag, :author_date)",
-                               data)
+            cursor_1.execute("SELECT author_date_unix_timestamp FROM hadoop_hash_tag_date WHERE id = %d", loop_num+1)
+            timestamp_target_tuple = cursor_1.fetchone()
+            timestamp_target = timestamp_target_tuple[0]
+            cursor_2.execute("UPDATE data SET author_date_flag = %d WHERE author_date_unix_timestamp < %s"
+                             , loop_num+1, timestamp_target)
 
         except sqlite3.Error as e:
             print('sqlite3.Error occurred:', e.args[0])
@@ -35,3 +43,10 @@ def slice_db():
 
 
 slice_db()
+
+conn_1.commit()
+conn_2.commit()
+
+# 接続を閉じる
+conn_1.close()
+conn_2.close()
